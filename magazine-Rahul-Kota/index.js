e565d19840bde52b746845109106ba9e20950b2a
RAHUL <53970626+rahulfrost@users.noreply.github.com>
2023-09-24 22:10:04 -0700

    Add files via upload

---
+++ magazine-Rahul-Kota/index.js
@@ -0,0 +1,8 @@
+let navbut = document.getElementById("navbutton")
+
+navbut.addEventListener("click", function()
+{
+    let navmenu = document.getElementById("navmenu")
+
+    navmenu.classList.toggle("shownav")
+})
